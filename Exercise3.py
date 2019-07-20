44bcd1b210cd51bc99aee0bd7ede8e849256d87c
toor <k.nosek@ievent-management.pl>
2019-07-20 19:15:59 +0000

    Which number is greater or equal

---
+++ Exercise3.py
@@ -0,0 +1,14 @@
+
+'''
+last_element([1,2,3]) # 3
+last_element([]) # None
+'''
+
+def last_element():
+    last_element([])
+    #last_element = ([1, 2, 3])
+    pop = last_element.pop()
+    return pop
+    pass
+
+print(last_element())
---
+++ Exercise4.py
@@ -0,0 +1,15 @@
+
+def number_compare(a,b):
+    '''
+    number_compare(1,1) # "Numbers are equal"
+    number_compare(1,0) # "First is greater"
+    number_compare(2,4) # "Second is greater"
+    '''
+    if a > b:
+        return "First is greater"
+    elif a < b:
+        return "Second is greater"
+    else:
+        return "Numbers are equal"
+    pass
+
