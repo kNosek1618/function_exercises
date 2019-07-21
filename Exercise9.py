d206e1ad5eba94d6853c849d81ecfde694cd7f8a
toor <k.nosek@ievent-management.pl>
2019-07-21 02:32:11 +0000

    frequency of occurs item in list

---
+++ Exercise9.py
@@ -0,0 +1,18 @@
+
+'''
+frequency([1,2,3,4,4,4], 4) # 3
+frequency([True, False, True, True], False) # 1
+'''
+
+def frequency(list, item):
+    return list.count(item)
+
+print(frequency())
+
+
+
+
+
+# x = [True, False, True, True]
+# frequencys = x.count(False)
+# print(frequencys)
